238640acef02a0b6fd3b2ba932fb229ba440c36d
Darkknight123 <philipkerry57@gmail.com>
2022-01-27 12:04:13 +0300

    added a second dice

--- app/src/main/java/com/example/DiceRollerApp/MainActivity.kt
+++ app/src/main/java/com/example/DiceRollerApp/MainActivity.kt
@@ -11,31 +11,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //hooks
+        /**
+         * hooks
+         */
         val rollButton: Button = findViewById(R.id.button)
 
-// when the button is clicked
+        /**
+         * when the button is clicked
+         */
+
         rollButton.setOnClickListener {
             rollDice()
-            val toast = Toast.makeText(this,"Dice Rolled",Toast.LENGTH_SHORT)
+            val toast = Toast.makeText(this, " 2 Dice Rolled", Toast.LENGTH_SHORT)
             toast.show()
         }
-   
+
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
 
-        resultTextView.text =diceRoll.toString()
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+        //Second dice display
+        val result2: TextView = findViewById(R.id.text)
+        result2.text = diceRoll2.toString()
     }
 }
 
 
-// Adding the dice roller logic code
-class Dice(private val numSides: Int){
-    fun roll() : Int{
+/**
+ * Adding the dice roller logic code
+ */
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
